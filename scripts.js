e1950093f9c4b919b9a319a55aeedcc7692330cf
Keith Gordon <a.keithgordon27@gmail.com>
2021-05-23 14:08:48 -0400

    initial commit with basic functionality

---
+++ scripts.js
@@ -0,0 +1,24 @@
+function add (x,y) {
+  return x + y;
+};
+console.log(add(1,2));
+
+function subtract(x,y) {
+  return x - y
+};
+console.log(subtract(10,5));
+
+function multiply (x,y) {
+  return x * y
+};
+console.log(multiply(10,10));
+
+function divide (x,y) {
+  return x / y
+};
+console.log(divide(20,10));
+
+function operate(operator, x, y) {
+  x = Number(x);
+  y = Number(y);
+}
